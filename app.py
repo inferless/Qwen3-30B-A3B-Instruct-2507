bb2db459e585d5a871c0e86ade30f65f1513dcf9
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-08-12 13:27:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from typing import Optional
 from pydantic import BaseModel, Field
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"]='1'
+
 @inferless.request
 class RequestObjects(BaseModel):
     prompt: str = Field(default="Explain reinforcement learning in simple words.")
